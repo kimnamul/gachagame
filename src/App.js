5c3e1d4ca3415355860f8db07ab791933dff01f8
yjkim01 <yjkim01@gitplecorp.com>
2022-08-24 16:11:19 +0900

    클릭으로 image animation on/off 적용

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import egg from './egg.png';
 import './App.css';
 
 
 function App() {
+  const [click, setClick] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={egg} className="App-logo" alt="logo" />
+        <img src={egg} className={click ? "App-logo" : "App-logo-active"} alt="logo" />
         <p>
-          Egg is spining...
+          Egg is spinning...
+        </p>
+        <p>
+          Hover to STOP spinning!
         </p>
       </header>
+      <main>
+        <button className="btn_spin" onClick={() => setClick(!click)}>
+          eeee
+        </button>
+      </main>
     </div>
   );
 }
