c6f94d652cc46a557fbd1ad460a0233d204c06a6
yjkim01 <yjkim01@gitplecorp.com>
2022-08-25 10:03:41 +0900

    goldclicker 변수 생성

--- src/NewApp.js
+++ src/NewApp.js
@@ -2,15 +2,16 @@ import { useState } from 'react';
 import './NewApp.css';
 
 function NewApp() {
+    // 보유 재화
     const [gold, setGold] = useState(0);
     const [maxgold, setMaxGold] = useState(1000);
-    const debug_earn_rate = 100;
     
     const [gem, setGem] = useState(0);
     const [maxgem, setMaxGem] = useState(99999);
     
+    // 재화 변동
     const EarnGold = (amount) => {
-        setGold(Math.min(gold + amount * debug_earn_rate, maxgold));
+        setGold(Math.min(gold + amount, maxgold));
     }
     const PayGold = (amount) => {
         if(gold < amount) {
@@ -26,6 +27,10 @@ function NewApp() {
     // 특정 수집품을 보유한다는 사실이 maxgold를 증가시켜줘야 자연스러움.
     // 현재는 특정 시점에 증가하는데, 수집품을 얻거나 레벨업 하는 시점에 증가하는건 뭔가뭔가임...
 
+    const debug_clicker_rate = 100; //디버그용 클리커 배율
+    const [goldclicker, setGoldClicker] = useState(1);
+    
+
     return (
         <div>
             <header>
@@ -37,7 +42,7 @@ function NewApp() {
                 <div className="clicker">
                     Here to be Clicker!
                     <br></br>
-                    <button className="gold_earn_button" onClick={() => EarnGold(1)}>
+                    <button className="gold_earn_button" onClick={() => EarnGold(goldclicker * debug_clicker_rate)}>
                         I Earn Gold
                     </button>
                     <br></br>
